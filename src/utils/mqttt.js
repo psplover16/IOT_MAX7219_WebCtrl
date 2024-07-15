3c32dbc6d6bf1757fdbcb5d68df210a67dd88c7a
gary_lin <gary_lin@cmoney.com.tw>
2024-07-15 17:20:29 +0800

    mqtt模組精簡

--- src/utils/mqttt.js
+++ src/utils/mqttt.js
@@ -1,46 +1,33 @@
 import mqtt from "mqtt";
 let mqttClient;
 
-function connect() {
-  return new Promise((resolve, reject) => {
-    try {
-      var brokerUrl = "mqtt://mqttgo.io:1883"; // 替换为 mqttgo.io 的地址
-      brokerUrl = "wss://broker.mqttgo.io:8084/mqtt";
-    //   
-      mqttClient = mqtt.connect(brokerUrl);
-      setTimeout(() => {
-        resolve(mqttClient);
-      }, 2000);
-    } catch (err) {
-      reject(new Error(err));
-    }
-  });
-}
-
-const connectMQTT = (func) => {
+const connectMQTT = (func,subTopic=['max7219-mqtt1'],brokerUrl = "wss://broker.mqttgo.io:8084/mqtt") => {
     return new Promise((resolve, reject) => {
-      connect()
-        .then((client) => {
-          client.subscribe('max7219-mqtt1'); // 連線
-          client.on('message', (topic, message) => { // 接收資訊
-            // 在收到消息後，可以進行相應的處理
-            func({ topic,  message });
-            resolve({ topic, message });
-          });
-        })
-        .catch((err) => {
-          console.log(err);
-          reject(err);
+      try {
+        // const brokerUrl = "wss://broker.mqttgo.io:8084/mqtt"; // 替換為 mqttgo.io 的地址
+        mqttClient = mqtt.connect(brokerUrl);
+  
+        mqttClient.on('connect', () => {
+            subTopic.forEach((v)=>{
+                mqttClient.subscribe(v);
+            })
+          resolve(mqttClient);
         });
+  
+        mqttClient.on('message', (topic, message) => {
+          func({ topic, message }); // 在收到消息後，可以進行相應的處理
+        });
+      } catch (err) {
+        reject(err);
+      }
     });
   };
 
 
-  const pubData = (topic,pubMsg) => {
-    mqttClient.publish(topic, pubMsg);
-  }
+const pubData = (topic,pubMsg) => {
+  mqttClient.publish(topic, pubMsg);
+}
 
+ 
 
-  
-
-  export {connectMQTT,pubData}
+export {connectMQTT,pubData}
